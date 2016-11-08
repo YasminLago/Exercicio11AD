282bf5aad598bb9c436920591b6a5d1a283d2b8d
YasminLago <ylagorebollar@danielcastelao.org>
2016-11-08 15:06:27 +0100

    Update Aleatorio.java

--- src/aleatorio/Aleatorio.java
+++ src/aleatorio/Aleatorio.java
@@ -11,30 +11,34 @@ import java.io.RandomAccessFile;
 public class Aleatorio {
     
     public static void main(String[] args) throws FileNotFoundException, IOException {
-    String [] codes ={"p1","p2","p3"};
-    String [] descricion = {"parafusos","cravos","tachas"};
-    double [] prices = {3.0,4.0,5.0};
     
-    RandomAccessFile ra = new RandomAccessFile("textoAleatorio.txt","rw");
-    Product p = new Product();
+        RandomAccessFile ra = new RandomAccessFile("textoAleatorio.txt","rw");
+        
+        String [] codes ={"p1","p2","p3"};
+        String [] descricion = {"parafusos","cravos","tachas"};
+        double [] prices = {3.0,4.0,5.0};
+        String cod = "";
+        String des = "";
     
-    for(int i=0;i<codes.length;i++){
-          //ra.seek(0);
-           ra.writeChars(String.format("%-10s",codes[i]));
-           //ra.write('\t');
-          
-           //ra.seek(34);
-           ra.writeChars(String.format("%-25s", descricion[i]));
-           //ra.write('\t');
-           
-        ra.writeDouble(prices[i]);
-        ra.close();
-   
-            
-           
-          System.out.println(codes[i]+descricion[i]);
+        //Se recorren las posiciones de los arrays
+        for(int i=0;i<codes.length;i++){
+           ra.writeChars(String.format("%-3s",codes[i]));
+           ra.writeChars(String.format("%-10s", descricion[i]));
+           ra.writeDouble(prices[i]); 
         }
-       
+        
+        ra.seek(34); //Posicion 2
+        for(int i = 0; i<13; i++){ // 13 = Numero de caracteres hasta donde se quiere leer
+            if(i<3){
+            cod +=  ra.readChar();
+            }else{
+            des +=  ra.readChar();
+            }
+        }
+        
+        Product p = new Product(cod,des,ra.readDouble());
+        System.out.println(p);
+          
         ra.close();
     }
         
